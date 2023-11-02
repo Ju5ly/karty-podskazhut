bd2c568e864933275e61fee94a05f014628a4662
Wintriss <zakharkinayua@gmail.com>
2023-11-02 22:16:51 +0300

    reset to initial commit

--- scripts/theme-changer.js
+++ scripts/theme-changer.js
@@ -1,39 +1,37 @@
-const lightThemeBtn = document.querySelector(
-  '.header__theme-button_type_light'
-);
-const darkThemeBtn = document.querySelector('.header__theme-button_type_dark');
+const lightThemeBtn = document.querySelector(".header__theme-button_type_light");
+const darkThemeBtn = document.querySelector(".header__theme-button_type_dark");
 
 window.onload = () => {
-  const themeInLocal = localStorage.getItem('theme');
-  if (themeInLocal) {
-    setDarkTheme();
-  } else {
-    setLightTheme();
-  }
+    const themeInLocal = localStorage.getItem("theme");
+    if (themeInLocal) {
+        setDarkTheme();
+    } else {
+        setLightTheme();
+    }
 };
 
 lightThemeBtn.onclick = () => {
-  setLightTheme();
-  localStorage.removeItem('theme');
+    setLightTheme();
+    localStorage.removeItem("theme");
 };
 
 darkThemeBtn.onclick = () => {
-  setDarkTheme();
-  localStorage.setItem('theme', 'dark');
+    setDarkTheme();
+    localStorage.setItem("theme", "dark");
 };
 
 function setDarkTheme() {
-  document.querySelector('.page').classList.add('theme_dark');
-  lightThemeBtn.classList.remove('header__theme-button_is-active');
-  darkThemeBtn.classList.add('header__theme-button_is-active');
-  darkThemeBtn.disabled = true;
-  lightThemeBtn.disabled = false;
+    document.querySelector(".page").classList.add("theme_dark");
+    lightThemeBtn.classList.remove(".header__theme-button_type_dark");
+    darkThemeBtn.classList.add("header__theme-button_is-active");
+    darkThemeBtn.disabled = true;
+    lightThemeBtn.disabled = false;
 }
 
 function setLightTheme() {
-  document.querySelector('.page').className = 'page';
-  darkThemeBtn.classList.remove('header__theme-button_is-active');
-  lightThemeBtn.classList.add('header__theme-button_is-active');
-  darkThemeBtn.disabled = false;
-  lightThemeBtn.disabled = true;
+    document.querySelector(".page").className = "page";
+    darkThemeBtn.classList.remove("header__theme-button_is-active");
+    lightThemeBtn.classList.add("header__theme-button_is-active");
+    darkThemeBtn.disabled = false;
+    lightThemeBtn.disabled = true;
 }
